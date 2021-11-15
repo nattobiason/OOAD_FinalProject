186379d99be36fb43bc9cfacdf8bb769b067905f
nattobiason <nato0933@colorado.edu>
2021-11-15 11:53:17 -0700

    created web interface

---
+++ web_interface/Cards/card.js
@@ -0,0 +1,10 @@
+class Card{
+    constructor(img, title, description, longDescription){
+        this.img = img;
+        this.title = title;
+        this.description = description;
+        this.longDescription = longDescription;
+    }
+}
+
+let Card = new Card("imgLink", "Title", "description", "a longer description")
