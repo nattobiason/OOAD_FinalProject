803089f01c45eb9c2137a3d56785975f213939bc
niketh49 <nigo5736@colorado.edu>
2021-12-08 17:57:46 -0700

    Incomplete

---
+++ web_interface/Games/BrickBreaker/src/paddle.js
@@ -0,0 +1,17 @@
+export default class Paddle {
+    constructor(gameWidth, gameHeight) {
+        
+        this.width = 150;
+
+        this.height= 30;
+        
+        this.position = {
+            x: gameWidth/2 - this.width/2,
+            y: gameHeight - this.height - 10,
+        };
+    }
+    draw(context) {
+        context.fillStyle = "#0ff";
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
